0be43530113b168668f658b27fd9c9c5f79d0000
DenisWm <denismamoni@hotmail.com>
2022-09-28 17:26:15 -0400

    Adjusting WorkerRepository

--- src/main/java/br/com/sea/repositories/WorkerRepository.java
+++ src/main/java/br/com/sea/repositories/WorkerRepository.java
@@ -2,13 +2,14 @@ package br.com.sea.repositories;
 
 import br.com.sea.entities.Sector;
 import br.com.sea.entities.Worker;
+import br.com.sea.entities.pk.WorkerPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface WorkerRepository extends JpaRepository<Worker, Integer> {
+public interface WorkerRepository extends JpaRepository<Worker, WorkerPK> {
 
     public Optional<Worker> findByCpf(String cpf);
     public void deleteByCpf(String cpf);
